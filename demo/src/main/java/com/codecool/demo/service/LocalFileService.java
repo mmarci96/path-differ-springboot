c04790c8ed1cac2cc6f9a214f523f59736d63fb3
mmarci96 <sarosdimarci@gmail.com>
2025-06-07 20:26:02 +0200

    reading recursively the files, logging if dir found and calling it. then log if file found

--- demo/src/main/java/com/codecool/demo/service/LocalFileService.java
+++ demo/src/main/java/com/codecool/demo/service/LocalFileService.java
@@ -6,6 +6,8 @@ import com.codecool.demo.repository.LocalFileRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
+
 @Service
 public class LocalFileService {
     private final DirectoryRepository directoryRepository;
@@ -21,7 +23,36 @@ public class LocalFileService {
     public boolean compareFiles(String pathA, String pathB) {
         System.out.println("PathA: " + pathA);
         System.out.println("PathB: " + pathB);
+        File file1 = new File(pathA);
+        processFile(file1);
+        // File file2 = new File(pathB);
+        // printFileDetails(file1);
+        // printFileDetails(file2);
+
         return true;
     }
 
+    private void processFile(File file){
+        // File file = new File(path);
+        if (file.isDirectory()){
+            File[] files = file.listFiles();
+            System.out.println("Directory found: "+file.getName());
+            for (File currFile : files) {
+               processFile(currFile); 
+            }
+        } else if (file.isFile()) {
+            printFileDetails(file);
+        }
+    }
+
+    private void printFileDetails(File file) {
+        System.out.println("+----------------+--------------------------+");
+        System.out.printf("| %-14s | %-24s |\n", "Property", "Value");
+        System.out.println("+----------------+--------------------------+");
+        System.out.printf("| %-14s | %-24s |\n", "Name", file.getName());
+        System.out.printf("| %-14s | %-24d |\n", "Size (bytes)", file.length());
+        System.out.printf("| %-14s | %-24s |\n", "Is Directory", file.isDirectory());
+        System.out.printf("| %-14s | %-24s |\n", "Is File", file.isFile());
+        System.out.println("+----------------+--------------------------+");
+    }
 }
