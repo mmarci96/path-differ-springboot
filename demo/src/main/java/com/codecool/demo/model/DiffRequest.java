38c90cfd1461c568683a303fa3372183eeb35bd5
mmarci96 <sarosdimarci@gmail.com>
2025-06-08 12:50:59 +0200

    history with get-diff working together

--- demo/src/main/java/com/codecool/demo/model/DiffRequest.java
+++ demo/src/main/java/com/codecool/demo/model/DiffRequest.java
@@ -1,8 +1,5 @@
 package com.codecool.demo.model;
 
-import com.codecool.demo.dto.DiffResponseDTO;
-import com.codecool.demo.dto.EntryDTO;
-import com.codecool.demo.dto.HistoryEntryDTO;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -16,11 +13,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @Entity
 @Table(name = "diff_request")
@@ -48,71 +40,5 @@ public class DiffRequest {
         this.localFileA = localFileA;
         this.localFileB = localFileB;
         this.createdAt = LocalDateTime.now();
-    }
-
-    public DiffResponseDTO getResponseDTO() {
-        Set<EntryDTO> sameFiles = new HashSet<>();
-
-        Set<EntryDTO> onlyPathA = new HashSet<>();
-        Set<EntryDTO> onlyPathB = new HashSet<>();
-
-        if (localFileA instanceof Directory dirA && localFileB instanceof Directory dirB) {
-            sameFiles.addAll(
-                    compareDirs(dirA, dirB).entrySet().stream()
-                            .map(e -> new EntryDTO(e.getKey(), e.getValue().getSize()))
-                            .collect(Collectors.toSet()));
-        } else {
-            var matchingFile = compareFiles(localFileA, localFileB);
-            if (matchingFile != null) {
-                sameFiles.add(new EntryDTO(matchingFile.name(), matchingFile.size()));
-            } else {
-                onlyPathA.add(new EntryDTO(localFileA.getName(), localFileA.getSize()));
-                onlyPathB.add(new EntryDTO(localFileB.getName(), localFileB.getSize()));
-            }
-        }
-
-        return new DiffResponseDTO(
-                localFileA.getPath(), localFileB.getPath(), onlyPathA, onlyPathB, sameFiles);
-    }
-
-    private EntryDTO compareFiles(LocalFile fileA, LocalFile fileB) {
-        if (!fileA.getName().equals(fileB.getName()) || fileB.getSize() != fileA.getSize()) {
-            return null;
-        }
-        return new EntryDTO(fileB.getName(), fileA.getSize());
-    }
-
-    private Map<String, LocalFile> compareDirs(Directory dirA, Directory dirB) {
-        Map<String, LocalFile> sameFiles = new HashMap<>();
-
-        Map<String, LocalFile> mapA =
-                dirA.getLocalFiles().stream().collect(Collectors.toMap(LocalFile::getName, f -> f));
-
-        Map<String, LocalFile> mapB =
-                dirB.getLocalFiles().stream().collect(Collectors.toMap(LocalFile::getName, f -> f));
-
-        for (String name : mapA.keySet()) {
-            LocalFile fileA = mapA.get(name);
-            LocalFile fileB = mapB.get(name);
-
-            if (fileB == null) continue;
-
-            if (fileA instanceof Directory subDirA && fileB instanceof Directory subDirB) {
-                // Recurse into subdirectories
-                sameFiles.putAll(compareDirs(subDirA, subDirB));
-            } else if (!(fileA instanceof Directory) && !(fileB instanceof Directory)) {
-                // Both are files
-                if (fileA.getSize() == fileB.getSize()) {
-                    sameFiles.put(fileA.getPath(), fileA); // optionally use fileA.getName()
-                }
-            }
-        }
-
-        return sameFiles;
-    }
-
-    public HistoryEntryDTO toHistoryDTO() {
-        var diffResp = getResponseDTO();
-        return new HistoryEntryDTO(username, diffResp, createdAt);
     }
 }
--- demo/src/main/java/com/codecool/demo/service/LocalFileService.java
+++ demo/src/main/java/com/codecool/demo/service/LocalFileService.java
@@ -12,6 +12,7 @@ import com.codecool.demo.util.LocalFileReader;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
@@ -30,47 +31,92 @@ public class LocalFileService {
     }
 
     public List<HistoryEntryDTO> getHistory() {
-        return diffRequestRepository.findAll().stream().map(DiffRequest::toHistoryDTO).toList();
+        List<HistoryEntryDTO> entryList = new ArrayList<HistoryEntryDTO>();
+        List<DiffRequest> diffRequests = diffRequestRepository.findAll().stream().toList();
+        for (DiffRequest request : diffRequests) {
+            LocalFile localFileA = request.getLocalFileA();
+            LocalFile localFileB = request.getLocalFileB();
+            DiffResponseDTO diff = compareFiles(localFileA, localFileB);
+
+            var name = request.getUsername();
+            var at = request.getCreatedAt();
+            HistoryEntryDTO entryDTO = new HistoryEntryDTO(name, diff, at);
+            entryList.add(entryDTO);
+        }
+
+        return entryList;
     }
 
-    public DiffResponseDTO compareFiles(String username, String pathA, String pathB) {
+    public DiffResponseDTO getDiffHandler(String username, String pathA, String pathB) {
         LocalFile localFileA = fileReader.readFileTree(pathA);
         LocalFile localFileB = fileReader.readFileTree(pathB);
+        DiffRequest request = new DiffRequest(username, localFileA, localFileB);
+        diffRequestRepository.save(request);
+        return compareFiles(localFileA, localFileB);
+    }
+
+    public DiffResponseDTO compareFiles(LocalFile localFileA, LocalFile localFileB) {
+        String pathA = localFileA.getPath();
+        String pathB = localFileB.getPath();
 
         Set<EntryDTO> sharedFiles = new HashSet<>();
         Set<EntryDTO> onlyInA = new HashSet<>();
         Set<EntryDTO> onlyInB = new HashSet<>();
 
         if (localFileA instanceof Directory dirA && localFileB instanceof Directory dirB) {
-
             Map<String, LocalFile> filesA = dirA.getAllNestedFilesWithRelativePaths("");
             Map<String, LocalFile> filesB = dirB.getAllNestedFilesWithRelativePaths("");
 
-            Set<String> allPaths = new HashSet<>(filesA.keySet());
-            allPaths.addAll(filesB.keySet());
-
-            for (String path : allPaths) {
-                LocalFile fileA = filesA.get(path);
-                LocalFile fileB = filesB.get(path);
-
-                if (fileA != null && fileB != null) {
-                    if (fileA.getSize() == fileB.getSize()) {
-                        sharedFiles.add(new EntryDTO(path, fileA.getSize()));
-                    } else {
-                        onlyInA.add(new EntryDTO(path, fileA.getSize()));
-                        onlyInB.add(new EntryDTO(path, fileB.getSize()));
-                    }
-                } else if (fileA != null) {
-                    onlyInA.add(new EntryDTO(path, fileA.getSize()));
-                } else {
-                    onlyInB.add(new EntryDTO(path, fileB.getSize()));
-                }
+            if (filesA.size() <= filesB.size()) {
+                classifyDifferences(filesA, filesB, sharedFiles, onlyInA, onlyInB);
+            } else {
+                classifyDifferences(filesB, filesA, sharedFiles, onlyInB, onlyInA);
             }
-
             return new DiffResponseDTO(pathA, pathB, onlyInA, onlyInB, sharedFiles);
         }
 
-        // fallback for non-directory files
+        Set<EntryDTO> fallbackShared = handleFallback(localFileA, localFileB, onlyInA, onlyInB);
+
+        return new DiffResponseDTO(pathA, pathB, onlyInA, onlyInB, fallbackShared);
+    }
+
+    private void classifyDifferences(
+            Map<String, LocalFile> filesA,
+            Map<String, LocalFile> filesB,
+            Set<EntryDTO> shared,
+            Set<EntryDTO> onlyInA,
+            Set<EntryDTO> onlyInB) {
+
+        for (Map.Entry<String, LocalFile> entry : filesA.entrySet()) {
+            String path = entry.getKey();
+            LocalFile fileA = entry.getValue();
+            LocalFile fileB = filesB.get(path);
+
+            if (fileB != null) {
+                if (fileA.getSize() == fileB.getSize()) {
+                    shared.add(new EntryDTO(path, fileA.getSize()));
+                } else {
+                    onlyInA.add(new EntryDTO(path, fileA.getSize()));
+                    onlyInB.add(new EntryDTO(path, fileB.getSize()));
+                }
+            } else {
+                onlyInA.add(new EntryDTO(path, fileA.getSize()));
+            }
+        }
+
+        for (Map.Entry<String, LocalFile> entry : filesB.entrySet()) {
+            String path = entry.getKey();
+            if (!filesA.containsKey(path)) {
+                onlyInB.add(new EntryDTO(path, entry.getValue().getSize()));
+            }
+        }
+    }
+
+    private Set<EntryDTO> handleFallback(
+            LocalFile localFileA,
+            LocalFile localFileB,
+            Set<EntryDTO> onlyInA,
+            Set<EntryDTO> onlyInB) {
         Set<EntryDTO> fallbackShared = new HashSet<>();
         if (localFileA.getName().equals(localFileB.getName())
                 && localFileA.getSize() == localFileB.getSize()) {
@@ -79,92 +125,6 @@ public class LocalFileService {
             onlyInA.add(new EntryDTO(localFileA.getName(), localFileA.getSize()));
             onlyInB.add(new EntryDTO(localFileB.getName(), localFileB.getSize()));
         }
-
-        return new DiffResponseDTO(pathA, pathB, onlyInA, onlyInB, fallbackShared);
+        return fallbackShared;
     }
-    //
-    // public void collectDifferences(
-    //     LocalFile fileA, LocalFile fileB, DiffRequest request, String relativePath) {
-    //   if (handleMissingFile(fileA, fileB, request, relativePath)) return;
-    //   if (handleFileMismatch(fileA, fileB, request, relativePath)) return;
-    //
-    //   if (fileA instanceof Directory dirA && fileB instanceof Directory dirB) {
-    //     compareDirectories(dirA, dirB, request, relativePath);
-    //   } else {
-    //     compareFiles(fileA, fileB, request, relativePath);
-    //   }
-    // }
-    //
-    // private boolean handleMissingFile(
-    //     LocalFile fileA, LocalFile fileB, DiffRequest request, String path) {
-    //   if (fileA == null && fileB == null) return true;
-    //
-    //   if (fileA == null || fileB == null) {
-    //     DiffEntry diff = new DiffEntry();
-    //     diff.setPath(path);
-    //     diff.setType("Missing");
-    //     diff.setMessage(fileA == null ? "Missing in A" : "Missing in B");
-    //     request.addDifference(diff);
-    //     return true;
-    //   }
-    //   return false;
-    // }
-    //
-    // private boolean handleFileMismatch(
-    //     LocalFile fileA, LocalFile fileB, DiffRequest request, String path) {
-    //   if (fileA.getClass() != fileB.getClass()) {
-    //     DiffEntry diff = new DiffEntry();
-    //     diff.setPath(path);
-    //     diff.setType("TypeMismatch");
-    //     diff.setMessage("Type mismatch: " + fileA.getName() + " vs " + fileB.getName());
-    //     request.addDifference(diff);
-    //     return true;
-    //   }
-    //   if (!fileA.getName().equals(fileB.getName())) {
-    //     DiffEntry diff = new DiffEntry();
-    //     diff.setPath(path);
-    //     diff.setType("NameMismatch");
-    //     diff.setMessage("Name mismatch: " + fileA.getName() + " vs " + fileB.getName());
-    //     request.addDifference(diff);
-    //     return true;
-    //   }
-    //
-    //   return false;
-    // }
-    //
-    // private void compareFiles(LocalFile fileA, LocalFile fileB, DiffRequest request, String path)
-    // {
-    //   if (fileA.getSize() != fileB.getSize()) {
-    //     DiffEntry diff = new DiffEntry();
-    //     diff.setPath(path);
-    //     diff.setType("SizeMismatch");
-    //     diff.setMessage("Size differs: " + fileA.getSize() + " vs " + fileB.getSize());
-    //     request.addDifference(diff);
-    //   }
-    // }
-    //
-    // private void compareDirectories(
-    //     Directory dirA, Directory dirB, DiffRequest request, String basePath) {
-    //   Map<String, LocalFile> mapB =
-    //       dirB.getLocalFiles().stream().collect(Collectors.toMap(LocalFile::getName, f -> f));
-    //
-    //   for (LocalFile childA : dirA.getLocalFiles()) {
-    //     String childPath = basePath + "/" + childA.getName();
-    //     LocalFile childB = mapB.get(childA.getName());
-    //     collectDifferences(childA, childB, request, childPath);
-    //   }
-    //
-    //   Set<String> namesInA =
-    //       dirA.getLocalFiles().stream().map(LocalFile::getName).collect(Collectors.toSet());
-    //
-    //   for (LocalFile childB : dirB.getLocalFiles()) {
-    //     if (!namesInA.contains(childB.getName())) {
-    //       DiffEntry diff = new DiffEntry();
-    //       diff.setPath(basePath + "/" + childB.getName());
-    //       diff.setType("ExtraInB");
-    //       diff.setMessage("File exists in B but not in A");
-    //       request.addDifference(diff);
-    //     }
-    //   }
-    // }
 }
