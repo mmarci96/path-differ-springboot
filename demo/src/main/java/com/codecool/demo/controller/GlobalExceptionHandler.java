dfa2d95065fe7c4bf1200f79c1953195f9f56eb7
mmarci96 <sarosdimarci@gmail.com>
2025-06-08 02:57:10 +0200

    handle notfound

--- demo/src/main/java/com/codecool/demo/controller/GlobalExceptionHandler.java
+++ demo/src/main/java/com/codecool/demo/controller/GlobalExceptionHandler.java
@@ -1,29 +1,21 @@
 package com.codecool.demo.controller;
 
-import com.codecool.demo.exception.FileNotFoundException;
+import com.codecool.demo.exception.LocalFileNotFoundException;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(FileNotFoundException.class)
-    public ResponseEntity<?> handleFileNotFoundException(
-            FileNotFoundException fileNotFoundException) {
+    @ExceptionHandler(LocalFileNotFoundException.class)
+    public ResponseEntity<?> handleLocalFileNotFound(LocalFileNotFoundException ex) {
         Map<String, String> error = new HashMap<>();
-        error.put("error", fileNotFoundException.getMessage());
+        error.put("error", ex.getMessage());
         return ResponseEntity.status(404).body(error);
-    }
-
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<?> handleGeneralException(Exception ex) {
-        Map<String, String> error = new HashMap<>();
-        error.put("error", "Something went wrong" + ex.getMessage());
-        return ResponseEntity.badRequest().body(error);
     }
 }
